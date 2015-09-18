02cd80c0809c899ea0587cec3e77f22640757983
林涛 <lovcavil@hotmail.com>
2015-09-18 17:25:30 +0800

    1

---
+++ trsvel1/trsvel1/Tarvel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace travel
+{
+    class Node
+    {
+        string name;
+        TimeSpan deftime;
+
+        Link[] links;
+
+    }
+    class Link
+    {
+        string name;
+        float cost;
+        TimeSpan deftime;
+        Node[] nodes=new Node[2];
+    }
+    class City:Node
+    {
+           bool stayed;
+    }
+    class Resort : Node
+    {
+    }
+
+    class Highway : Link
+    {
+
+    }
+    class Road : Link
+    {
+
+    }
+}
---
+++ trsvel1/trsvel1/Trip.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace travel
+{
+    static class TravelPlan
+    {
+        static List<SingleTrip> possibleST = new List<SingleTrip>();
+        static void Travel()
+        {
+            List<State> activeStates=new List<State>();
+
+        }
+    }
+    class SingleTrip
+    {
+        int day = 1;
+        List<Act> pastActs;
+
+    }
+
+    class State
+    {
+        Node Location;
+        int day;
+        TimeSpan timeLeft;
+        List<Act> possbileAct = new List<Act>();
+
+        State(Node root)
+        {
+            Location = root;
+            day = 1;
+            timeLeft = TimeSpan.FromHours(12);
+        }
+
+        State(State cpy)
+        {
+            Location = cpy.Location;
+            day = cpy.day;
+            timeLeft = cpy.timeLeft;
+            List<Act> possbileAct = new List<Act>();
+        }
+
+        State Run(Act act)
+        {
+            var result=new State(this);
+            while (GetType().IsInstanceOfType(act))
+            {
+                ;
+            }
+            return result;
+        }
+
+       void SetPossbileAct()
+        {            
+            ;
+        }
+    }
+
+
+
+    class Move : Act
+    {
+        Node from;
+        Node to;
+    }
+    class Act
+    {
+
+    }
+}
